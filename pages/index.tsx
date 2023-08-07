fa17cbfd3706c64d7c07fe264b8c123f19bdfdde
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-08-07 18:17:19 -0500

    Protect route with session managment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,38 @@
-// Todo - complete home page
-export default function Home() {
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+const Home = () => {
+  const { data: user} = useCurrentUser();
+
   return (
-   <> 
-    <h1>Netflix Clone</h1> 
-   </>
+    <>
+      <p className='text-4xl'>Logged in as : {user?.email}</p>
+      <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
+        Sign out of Netflix
+      </div>
+    </>
   )
 }
+
+export default Home;
