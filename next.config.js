c53544c3419a8f5406846180483ac106dd08b0ef
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-08-25 11:58:13 -0500

    Exclude test files from the client-side build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Exclude test files from the client-side build
+    if (!isServer) {
+      config.module.rules.push({
+        test: /__tests__\/.*$/,
+        loader: 'ignore-loader',
+      });
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
